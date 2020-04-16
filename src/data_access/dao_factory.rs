660753979a9d50d5c157a9d5d18758e4bdfb7738
Cyriel Bastin <cyriel.bastin@zoho.com>
2020-04-17 00:00:32 +0200

    DAO Factory (JUST USE IT)

--- src/data_access/dao_factory.rs
+++ src/data_access/dao_factory.rs
@@ -1,5 +1,26 @@
 use super::DAOFactory;
+use crate::data_access::{DbConnection, DAOIngredient, DAOBoisson, DAOBurger, DAOCommande};
 
 impl DAOFactory
 {
+    pub fn create_dao_ingredient() -> DAOIngredient {
+        DAOIngredient {
+            conn: DbConnection::new()
+        }
+    }
+    pub fn create_dao_boisson() -> DAOBoisson {
+        DAOBoisson {
+            conn: DbConnection::new()
+        }
+    }
+    pub fn create_dao_burger() -> DAOBurger {
+        DAOBurger {
+            conn: DbConnection::new()
+        }
+    }
+    pub fn create_dao_commande() -> DAOCommande {
+        DAOCommande {
+            conn: DbConnection::new()
+        }
+    }
 }
