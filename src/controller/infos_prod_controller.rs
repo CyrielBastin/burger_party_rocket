519addb295772c416ebe3a4bb310e1b370e18548
Cyriel Bastin <cyriel.bastin@zoho.com>
2020-05-05 04:50:45 +0200

    details burgers back-end + debut of front end

--- src/controller/infos_prod_controller.rs
+++ src/controller/infos_prod_controller.rs
@@ -27,3 +27,33 @@ pub fn infos_boissons() -> Template
 
     Template::render("infos_produits/infos_boissons", context)
 }
+
+#[get("/burgers/burger/<id>")]
+pub fn details_burger(id: u32) -> Template
+{
+    let mut burger_repo = DAOFactory::create_dao_burger();
+    let burger = burger_repo.find_by_id(id);
+    let mut context = Context::new();
+
+    let mut calories: u16 = 0;
+    for ing in burger.get_ingredients()
+    {
+        calories += (*ing).get_calories() * (*ing).get_quantite() as u16;
+    }
+
+    context.insert("burger", &burger);
+    context.insert("calories", &calories);
+
+    Template::render("infos_produits/details_burgers", context)
+}
+
+#[get("/boissons/boisson/<id>")]
+pub fn details_boisson(id: u32) -> Template
+{
+    let mut boisson_repo = DAOFactory::create_dao_boisson();
+    let boisson = boisson_repo.find_by_id(id);
+    let mut context = Context::new();
+    context.insert("boisson", &boisson);
+
+    Template::render("infos_produits/details_boissons", context)
+}
--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,19 @@ mod controller;
 fn main()
 {
     rocket::ignite()
-        .mount("/", routes![controller::homepage_controller::index,
-                            controller::homepage_controller::qui_sommes_nous])
-        .mount("/public", routes![controller::public_resources_controller::get_css,
-                                  controller::public_resources_controller::get_font,
-                                  controller::public_resources_controller::get_image,
-                                  controller::public_resources_controller::get_js])
-        .mount("/informations-produits", routes![controller::infos_prod_controller::infos_burgers,
-                                                 controller::infos_prod_controller::infos_boissons])
+        .mount("/", routes![
+            controller::homepage_controller::index,
+            controller::homepage_controller::qui_sommes_nous])
+        .mount("/public", routes![
+            controller::public_resources_controller::get_css,
+            controller::public_resources_controller::get_font,
+            controller::public_resources_controller::get_image,
+            controller::public_resources_controller::get_js])
+        .mount("/informations-produits", routes![
+            controller::infos_prod_controller::infos_burgers,
+            controller::infos_prod_controller::infos_boissons,
+            controller::infos_prod_controller::details_burger,
+            controller::infos_prod_controller::details_boisson])
         .attach(Template::fairing())
         .launch();
 }
