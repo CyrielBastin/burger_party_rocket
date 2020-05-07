8e00410cf1b6a2b272006ba1267b9e62cccd6690
Cyriel Bastin <cyriel.bastin@zoho.com>
2020-05-07 02:41:10 +0200

    changes to tabs 'burger and drink'

--- src/controller/commande_controller.rs
+++ src/controller/commande_controller.rs
@@ -1,5 +1,6 @@
 use rocket_contrib::templates::Template;
 use rocket_contrib::templates::tera::Context;
+use crate::data_access::{DAOFactory, DAO};
 
 //==================================================================================================
 // All routes ares prefixed with /commande
@@ -8,7 +9,11 @@ use rocket_contrib::templates::tera::Context;
 #[get("/new")]
 pub fn commande_new() -> Template
 {
-    let context = Context::new();
+    let mut burger_repo = DAOFactory::create_dao_burger();
+    let list_burgers = burger_repo.find_all();
+
+    let mut context = Context::new();
+    context.insert("burgers", &list_burgers);
 
     Template::render("commande/commande_new", context)
 }
