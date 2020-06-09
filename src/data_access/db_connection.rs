0c5a115dd073a29b12cac9f570ea3f8cee4dd0f9
Cyriel Bastin <cyriel.bastin@zoho.com>
2020-06-09 20:42:41 +0200

    moved database config to a different file

--- src/data_access/db_connection.rs
+++ src/data_access/db_connection.rs
@@ -1,16 +1,24 @@
 use super::DbConnection;
 use mysql::{PooledConn, OptsBuilder, Pool};
+use std::fs::File;
+use std::io::Read;
+use serde::{Serialize, Deserialize};
 
 impl DbConnection<'_>
 {
     pub fn new() -> PooledConn
     {
+        let mut database_json = File::open("./database.json").unwrap();
+        let mut file_content = String::new();
+        database_json.read_to_string(&mut file_content).unwrap();
+        let db_config: ConfigFromFile = serde_json::from_str(&file_content).unwrap();
+
         let mut db_conn = DbConnection {
-            ip: "127.0.0.1",
-            user: "root",
-            password: "",
-            database: "fastfood",
-            port: 3308,
+            ip: db_config.ip,
+            user: db_config.user,
+            password: db_config.password,
+            database: db_config.database,
+            port: db_config.port,
             pool: None
         };
         let opts_builder = OptsBuilder::new()
@@ -25,4 +33,14 @@ impl DbConnection<'_>
         let conn = db_conn.pool.unwrap();
         conn.get_conn().unwrap()
     }
+}
+
+#[derive(Serialize, Deserialize)]
+struct ConfigFromFile<'a>
+{
+    ip: &'a str,
+    user: &'a str,
+    password: &'a str,
+    database: &'a str,
+    port: u16,
 }
